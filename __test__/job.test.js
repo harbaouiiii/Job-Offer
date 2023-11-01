7d3f58635ac4f4440306ac2379f0b30451ddc47e
Mohamed HARBAOUI <harbeouimohamed@gmail.com>
2023-11-01 12:32:48 +0100

    added unit test

---
+++ __test__/job.test.js
@@ -0,0 +1,87 @@
+const request = require("supertest");
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const { createServer } = require("../utils/serverUtils");
+
+const app = createServer();
+
+describe("Job", () => {
+    jest.setTimeout(30000);
+    let authToken;
+
+    beforeAll(async () => {
+        const mongoServer = await MongoMemoryServer.create();
+        await mongoose.connect(mongoServer.getUri());
+
+        const mockUser = {
+            _id: new mongoose.Types.ObjectId(), // Generate a new ObjectId
+            email: "test@example.com",
+            password: "123456789"
+        };
+
+        authToken = jwt.sign({ id: mockUser._id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRE
+        });
+    });
+
+    afterAll(async () => {
+        await mongoose.disconnect();
+        await mongoose.connection.close();
+    });
+
+
+    test("should not create a job when user not authorised", async () => {
+        await request(app)
+            .post("/api/job")
+            .send({})
+            .expect(401);
+    });
+
+    test("should not create an empty job", async () => {
+        await request(app)
+            .post("/api/job")
+            .set('Authorization', `Bearer ${authToken}`)
+            .send({})
+            .expect(400);
+    });
+
+    test("should create a job", async () => {
+        const user = {
+            firstName: "Mohamed",
+            lastName: "HARBAOUI",
+            email: "harbeouimohamed@gmail.com",
+            role: "CANDIDATE",
+            password: "123456789",
+            dateOfBirth: Date.UTC(1997, 7, 16)
+        }
+        const job = {
+            name: "Software Developer",
+            description: "We are looking for a skilled software developer...",
+            company: "Tech Company XYZ",
+            location: "Tunisia",
+            salary: 5000,
+            contract: "CDI",
+            createdBy: user
+        };
+
+        await request(app)
+            .post("/api/job")
+            .set('Authorization', `Bearer ${authToken}`)
+            .send(job)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).toBeTruthy();
+                expect(response.body.name).toBe(job.name);
+                expect(response.body.description).toBe(job.description);
+                expect(response.body.company).toBe(job.company);
+                expect(response.body.location).toBe(job.location);
+                expect(response.body.salary).toBe(job.salary);
+                expect(response.body.contract).toBe(job.contract);
+                expect(response.body.createdBy).toBe(job.createdBy);
+            });
+    });
+
+});
